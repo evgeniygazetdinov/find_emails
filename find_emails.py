ee98cc6b9e8fdab420bb6788f015db62eba887e1
evgesha <zplacesound@gmail.com>
2020-07-02 09:03:58 +0300

    add finder_emails

--- find_emails.py
+++ find_emails.py
@@ -5,6 +5,35 @@ from collections import OrderedDict
 from writecsv import write_to_csv,FILE
 import numpy as np
 from collections import namedtuple
+import re
+import xlwt
+import pandas as pd
+from openpyxl import load_workbook
+import os
+filename  = "emails.xlsx"
+
+
+def create_file():
+	writer = pd.ExcelWriter(filename, engine='xlsxwriter')
+	writer.save()
+
+	df = pd.DataFrame({'link': [],
+					'email': []})
+	writer = pd.ExcelWriter(filename, engine='xlsxwriter')
+
+	df.to_excel(writer, sheet_name='Sheet1', index=False)
+	writer.save()
+
+def to_file(emails,link):
+	df = pd.DataFrame({'link': [link],'email': [emails]})
+	writer = pd.ExcelWriter(filename, engine='openpyxl')
+	writer.book = load_workbook(filename)
+	writer.sheets = dict((ws.title, ws) for ws in writer.book.worksheets)
+	reader = pd.read_excel(r'{}'.format(filename))
+	df.to_excel(writer,index=False,header=False,startrow=len(reader)+1)
+	writer.close()
+
+
 
 def clean_name(name):
 	if name is not None:
@@ -14,28 +43,50 @@ def clean_name(name):
 	return None
 
 
-def read_links_from_csv():
-	lines = np.genfromtxt(FILE, delimiter=",", dtype=None)
+def read_links_from_csv(file):
+	lines = np.genfromtxt(file, delimiter=",", dtype=None)
 	my_dict = dict()
 	for i in range(len(lines)):
 	   my_dict[lines[i][0]] = lines[i][1]
 	return my_dict
 
 
-def check_link_on_email(link):
-	pass
+def emailExtractor(link):
+	getH=requests.get(link)
+	h=getH.content
+	soup=BeautifulSoup(h,'html.parser')
+	mailtos = soup.select('a[href^=mailto]')
+	for i in mailtos:
+		href=i['href']
+		try:
+			str1, str2 = href.split(':')
+		except ValueError:
+			break
+		
+		return str2
 
+
+
+
+
+def check_link_on_email(link):
+	if not os.path.exists(filename):
+		create_file()
+	email = emailExtractor(link)
+
+	if email is not None:
+		to_file(email,link)
 
 
 
 
 def check_links(links):
-	emails_with_names = namedtuple()
-	for name_studio,link in links.items():
-		email = check_link_on_email(link)
-		if email is None:
-			continue
-        emails_with_names[name_studio] = email
+	emails_with_names = OrderedDict()
+	for studio_address,studio_name in links.items():
+		if not re.match(r'http',studio_address):
+				continue
+		email = check_link_on_email(studio_address)
+		emails_with_names[studio_name] = email
 	return emails_with_names
 
 
@@ -61,13 +112,16 @@ def find_links():
 					res['url'] = url
 					res['studio_name'] = name_without_tags
 					write_to_csv(res)
+					print(res)
 				else:
 					continue
 
 def find_emails_on_links():
-	links = read_links_from_csv()
-	emails = check_links(email)
+	links = read_links_from_csv('out.csv')
+	emails = check_links(links)
 
 
 if __name__ == '__main__':
-	find_links()
+    #if out not exists execute find_links	
+	find_emails_on_links()
+	
